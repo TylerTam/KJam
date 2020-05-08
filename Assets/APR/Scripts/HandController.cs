4ebfec43ec21a70b9216f3eae82a5de3dd4c08e3
TylerTam <46612488+TylerTam@users.noreply.github.com>
2020-05-08 13:14:02 -0400

    Initial Commit
    -Added Ragdoll Asset Pack
    -Added Rewired
    -Added right and left movement to the ragdoll

---
+++ Assets/APR/Scripts/HandController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class HandController : MonoBehaviour
+{
+    public APRController APR_Player;
+    public bool Left;
+    public bool hasJoint;
+    private bool hasWaitedAfterThrow = true;
+
+    bool m_pickupInputDown;
+
+
+    public void ChangeHandInput(bool p_inputDown)
+    {
+        m_pickupInputDown = p_inputDown;
+
+        if (hasJoint && !p_inputDown)
+        {
+            this.gameObject.GetComponent<FixedJoint>().breakForce = 0;
+            hasJoint = false;
+            hasWaitedAfterThrow = false;
+        }
+
+        if (hasJoint && this.gameObject.GetComponent<FixedJoint>() == null)
+        {
+            hasJoint = false;
+            hasWaitedAfterThrow = false;
+        }
+    }
+
+
+
+    //Grab on collision
+    void OnCollisionEnter(Collision col)
+    {
+        if (APR_Player.useControls && hasWaitedAfterThrow)
+        {
+            //Left Hand
+            if (Left)
+            {
+                if (col.gameObject.tag == "Object" && !hasJoint)
+                {
+                    if (m_pickupInputDown && !hasJoint)
+                    {
+                        hasJoint = true;
+                        hasWaitedAfterThrow = false;
+                        this.gameObject.AddComponent<FixedJoint>();
+                        this.gameObject.GetComponent<FixedJoint>().breakForce = 100000;
+                        this.gameObject.GetComponent<FixedJoint>().connectedBody = col.gameObject.GetComponent<Rigidbody>();
+                    }
+                }
+
+                else if (col.gameObject.tag == "Player" && col.gameObject.layer != LayerMask.NameToLayer(APR_Player.thisPlayerLayer) && !hasJoint)
+                {
+                    if (m_pickupInputDown && !hasJoint)
+                    {
+                        hasJoint = true;
+                        hasWaitedAfterThrow = false;
+                        this.gameObject.AddComponent<FixedJoint>();
+                        this.gameObject.GetComponent<FixedJoint>().breakForce = 100000;
+                        this.gameObject.GetComponent<FixedJoint>().connectedBody = col.gameObject.GetComponent<Rigidbody>();
+                    }
+                }
+            }
+        }
+    }
+
+    void OnJointBreak()
+    {
+        StartCoroutine(DelayCoroutine());
+        IEnumerator DelayCoroutine()
+        {
+            yield return new WaitForSeconds(1f);
+            hasWaitedAfterThrow = true;
+        }
+    }
+}
---
+++ Assets/_PandemicPanic/Scripts/PlayerController/PlayerInput.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+
+public class PlayerInput : MonoBehaviour
+{
+    public int m_playerId;
+
+    private APRController m_ragdollController;
+    private Player m_playerInputController;
+
+    public static PlayerInput Instance;
+    private void Awake()
+    {
+
+        Instance = this;
+    }
+    private void Start()
+    {
+
+        m_ragdollController = GetComponent<APRController>();
+        m_playerInputController = ReInput.players.GetPlayer(m_playerId);
+    }
+
+    public void ChangeCursorState(bool p_activeState)
+    {
+        Cursor.visible = !false;
+
+        if (!p_activeState)
+        {
+
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    private void Update()
+    {
+        GetInput();
+    }
+
+
+
+    public void GetInput()
+    {
+        #region Movement Input
+        if (m_playerInputController.GetButton("MoveForward"))
+        {
+            m_ragdollController.MoveForward(true);
+        }
+        if (m_playerInputController.GetButtonUp("MoveForward"))
+        {
+            m_ragdollController.MoveForward(false);
+        }
+
+        if (m_playerInputController.GetButton("MoveBackwards"))
+        {
+            m_ragdollController.MoveBackwards(true);
+        }
+        if (m_playerInputController.GetButtonUp("MoveBackwards"))
+        {
+            m_ragdollController.MoveBackwards(false);
+        }
+
+        if (m_playerInputController.GetButton("MoveLeft"))
+        {
+            m_ragdollController.MoveLeft(true);
+        }
+        if (m_playerInputController.GetButtonUp("MoveLeft"))
+        {
+            m_ragdollController.MoveLeft(false);
+        }
+
+        if (m_playerInputController.GetButton("MoveRight"))
+        {
+            m_ragdollController.MoveRight(true);
+        }
+        if (m_playerInputController.GetButtonUp("MoveRight"))
+        {
+            m_ragdollController.MoveRight(false);
+        }
+        #endregion
+
+        #region Jump
+        if (m_playerInputController.GetButtonDown("Jump"))
+        {
+            m_ragdollController.JumpInput(true);
+        }
+        #endregion
+
+        #region Arm Inputs
+        if (m_playerInputController.GetButtonDown("LeftPickup"))
+        {
+            m_ragdollController.LeftPickupInput(true);
+        }
+        if (m_playerInputController.GetButtonUp("LeftPickup"))
+        {
+            m_ragdollController.LeftPickupInput(false);
+        }
+
+        if (m_playerInputController.GetButtonDown("RightPickup"))
+        {
+            m_ragdollController.RightPickupInput(true);
+        }
+        if (m_playerInputController.GetButtonUp("RightPickup"))
+        {
+            m_ragdollController.RightPickupInput(false);
+        }
+
+        if (m_playerInputController.GetButtonDown("Throw"))
+        {
+            m_ragdollController.PickupAndThrow(true);
+        }
+
+        if (m_playerInputController.GetButtonDown("LeftPunch"))
+        {
+            m_ragdollController.PunchInput(false, true);
+        }
+        if (m_playerInputController.GetButtonDown("RightPunch"))
+        {
+            m_ragdollController.PunchInput(true, false);
+        }
+        #endregion
+    }
+
+}
