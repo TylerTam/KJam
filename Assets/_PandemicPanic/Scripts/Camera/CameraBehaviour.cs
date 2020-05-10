877833ed72adda297d3b6db49796c6ad685155ab
TylerTam <46612488+TylerTam@users.noreply.github.com>
2020-05-10 11:48:31 -0400

    Fixed Camera to zoom in properly

--- Assets/_PandemicPanic/Scripts/Camera/CameraBehaviour.cs
+++ Assets/_PandemicPanic/Scripts/Camera/CameraBehaviour.cs
@@ -6,11 +6,15 @@ public class CameraBehaviour : MonoBehaviour
 {
     public static CameraBehaviour Instance;
     public Camera m_camera;
-    private float m_startingCameraZ;
+
+    [Tooltip("Changing this will change the starting Camera postiion")]
     public float m_lowestCameraZoom;
+    public float m_farthestCameraZoon;
+    public float m_maxCharacterDistance;
+
     private List<Transform> m_players = new List<Transform>();
     public float m_cameraFieldOfView = 60;
-    public float m_lerpSpeed;
+    public float m_horizontalLerpSpeed, m_zoomLerpSpeed;
 
     private Vector3 m_cameraOffset;
     private float m_startingDistance;
@@ -19,7 +23,7 @@ public class CameraBehaviour : MonoBehaviour
     {
         Instance = this;
         m_cameraOffset = transform.position;
-        m_startingCameraZ = m_camera.transform.localPosition.z;
+        m_camera.transform.localPosition = new Vector3(0, 0, m_lowestCameraZoom);
     }
     public void AssignPlayer(List<GameObject> p_player)
     {
@@ -62,7 +66,8 @@ public class CameraBehaviour : MonoBehaviour
         Vector3 farthestPoint = Vector3.zero, secondFarthest = Vector3.zero;
         float farthestDistance = 0;
 
-        foreach(Transform pos in m_players)
+        #region Horizontal Movement Adjustment
+        foreach (Transform pos in m_players)
         {
             centerPoint += pos.position;
             if(Vector3.Distance(pos.position, Vector3.zero) > farthestDistance)
@@ -72,9 +77,13 @@ public class CameraBehaviour : MonoBehaviour
             }
         }
         centerPoint = centerPoint / m_players.Count;
+        Vector3 targetPos = new Vector3(centerPoint.x + m_cameraOffset.x, transform.position.y, centerPoint.z + m_cameraOffset.z);
+        transform.position = Vector3.Lerp(new Vector3(transform.position.x, transform.position.y, transform.position.z), targetPos, m_horizontalLerpSpeed * Time.deltaTime);
 
+        #endregion
+
+        #region Zoom Adjustment
         farthestDistance = 0;
-
         foreach (Transform pos in m_players)
         {
             if(Vector3.Distance(pos.position, farthestPoint) > farthestDistance)
@@ -86,13 +95,11 @@ public class CameraBehaviour : MonoBehaviour
             Debug.DrawLine(pos.position, centerPoint, Color.yellow);
         }
 
-        Vector3 targetPos = new Vector3(centerPoint.x + m_cameraOffset.x, transform.position.y, centerPoint.z + m_cameraOffset.z);
-
-        transform.position = Vector3.Lerp(new Vector3(transform.position.x, transform.position.y, transform.position.z), targetPos, m_lerpSpeed*Time.deltaTime);
+        
         Debug.DrawLine(transform.position, transform.forward * 1000, Color.green);
-        float percent = farthestDistance / m_startingDistance;
-        m_camera.transform.localPosition = new Vector3(0, 0, Mathf.Lerp(m_startingCameraZ, m_lowestCameraZoom, 1 - percent));
-
+        targetPos = new Vector3(0, 0, Mathf.Lerp(m_lowestCameraZoom, m_farthestCameraZoon, farthestDistance / m_maxCharacterDistance));
+        m_camera.transform.localPosition = Vector3.Lerp(m_camera.transform.localPosition, targetPos, m_zoomLerpSpeed * Time.deltaTime);
+        #endregion
 
     }
 }
