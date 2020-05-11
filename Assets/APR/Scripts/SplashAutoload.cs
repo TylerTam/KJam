b5e81101c8592e0dfd2de839ab36abbe3bf8f0f6
MSI\Wu <zhangwu@sheridancollege.ca>
2020-05-11 00:13:12 -0400

    StartSplash

---
+++ Assets/APR/Scripts/SplashAutoload.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashAutoload : MonoBehaviour
+{
+    public float CountDown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Countdown());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private IEnumerator Countdown()
+    {
+        float duration = CountDown;
+
+        float normalizedTime = 0;
+        while (normalizedTime <= duration)
+        {
+            normalizedTime += Time.deltaTime;
+            yield return null;
+        }
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
